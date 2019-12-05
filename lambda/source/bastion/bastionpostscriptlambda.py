c2791a7c955a8e050ec35b19f784156c7a628cbf
Dhruvit <dhpatel@ibaset.com>
2019-12-05 14:32:21 -0800

    kms and restructure updates

--- lambda/source/bastion/bastionpostscriptlambda.py
+++ lambda/source/bastion/bastionpostscriptlambda.py
@@ -98,47 +98,6 @@ def setup_bastion(event, context, stack_name):
             Tier='Standard'
         )
 
-        # You need to have user type on the password as the parameter in CFN and then pass the password to Lambda using ResourceProperties
-        # More help @ https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html
-        # You can pass value as event data from cfn and then get it from event object e.g. event['ResourceProperties']['Password']
-        rds_password = event['ResourceProperties']['RDSPassword']
-        
-        # Note that you must create a KMS Key that will be used to secure the ssm parameter. You can use CFN to create KMS Key and then pass in as event ResourceProperties
-        # More help @ https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html
-        # You can pass this KeyID as event data from cfn and then get it from event object e.g. event['ResourceProperties']['KMSKeyID']
-        kmskeyid = event['ResourceProperties']['KMSKeyID']
-        print("Creating ssm parameter RDS Password")
-        ssm_client = boto3.client('ssm')
-        # create a ssm parameter
-        response = ssm_client.put_parameter(
-            Name='SSDBAdminPassword',
-            Description='Password of the RDS DB Server Admin',
-            Value=rds_password,
-            KeyId=kmskeyid,
-            Type='SecureString',
-            Overwrite=True,
-            Tier='Standard'
-        )
-
-        # You need to have user type on the password as the parameter in CFN and then pass the password to Lambda using ResourceProperties
-        # More help @ https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html
-        # You can pass value as event data from cfn and then get it from event object e.g. event['ResourceProperties']['Password']
-        mongo_password = event['ResourceProperties']['MongoDBPassword']        
-        print("Creating ssm parameter Mongo Password")
-        ssm_client = boto3.client('ssm')
-        # create a ssm parameter
-        response = ssm_client.put_parameter(
-            Name='MongoDBAdminPassword',
-            Description='Password of the Mongo DB Server Admin',
-            Value=mongo_password,
-            KeyId=kmskeyid,
-            Type='SecureString',
-            Overwrite=True,
-            Tier='Standard'
-        )
-        
-        print("Finished creating ssm parameters for Mongo and RDS Password")
-
         ec2 = boto3.resource('ec2')
         instance = ec2.Instance(BastionInstanceID)
         iam_instance_profile = instance.iam_instance_profile
@@ -149,6 +108,7 @@ def setup_bastion(event, context, stack_name):
         # rolename = iam_instance_profile_arn.split('/')[len(iam_instance_profile_arn.split('/'))-1]
 
         rolename = ''
+        kms_policy_arns = []
         client_iam = boto3.client('iam')
         response_list_roles = client_iam.list_roles()
         Role_list = response_list_roles['Roles']
@@ -157,6 +117,7 @@ def setup_bastion(event, context, stack_name):
                 rolename = key['RoleName']
                 print('RoleName: ' + key['RoleName']) 
                 print('ARN: ' + key['Arn'])
+                kms_policy_arns.append(key['Arn']) 
         
                 print('rolename: ' + rolename)
                 
@@ -205,6 +166,90 @@ def setup_bastion(event, context, stack_name):
             ],
             MaxResults=123
         )
+
+
+        # Create KMS Keys and attach the IAM policy with required ARNs
+        sts = boto3.client('sts')
+        identity = sts.get_caller_identity()
+        
+        # Inputs
+        rds_role_name = event['ResourceProperties']['InstanceProfileRoleName']
+        
+        # role arns that need kms access
+        rds_role_arn = 'arn:aws:iam::' + identity['Account'] + ':role/' + rds_role_name
+        cfn_user_arn = identity['Arn']
+        root_arn = 'arn:aws:iam::' + identity['Account'] + ':root'
+
+        kms_policy_arns.append(rds_role_arn)
+        kms_policy_arns.append(cfn_user_arn)
+        kms_policy_arns.append(root_arn)
+        
+        print('user identity: ' + json.dumps(identity))
+        
+        kms_policy =  {
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Sid": "Enable IAM User Permissions for KMS Key",
+                                "Effect": "Allow",
+                                "Principal": {
+                                    "AWS": kms_policy_arns
+                                },
+                                "Action": "kms:*",
+                                "Resource": "*"
+                            }
+                        ]
+                      }
+        
+        client_kms = boto3.client('kms')
+        response = client_kms.create_key(
+            Description='KMS key to decyrpt the QuickStart credentials',
+            KeyUsage='ENCRYPT_DECRYPT',
+            Origin='AWS_KMS',
+            Policy=json.dumps(kms_policy)
+        )
+        
+        print ('Created KMS Key with ARN: ' + response['KeyMetadata']['Arn']);
+        print ('Created KMS Key with KeyID : ' + response['KeyMetadata']['KeyId']);
+
+        kmskeyid = response['KeyMetadata']['KeyId']
+        # You need to have user type on the password as the parameter in CFN and then pass the password to Lambda using ResourceProperties
+        # More help @ https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html
+        # You can pass value as event data from cfn and then get it from event object e.g. event['ResourceProperties']['Password']
+        rds_password = event['ResourceProperties']['RDSPassword']
+        
+        print("Creating ssm parameter RDS Password")
+        ssm_client = boto3.client('ssm')
+        # create a ssm parameter
+        response = ssm_client.put_parameter(
+            Name='SSDBAdminPassword',
+            Description='Password of the RDS DB Server Admin',
+            Value=rds_password,
+            KeyId=kmskeyid,
+            Type='SecureString',
+            Overwrite=True,
+            Tier='Standard'
+        )
+
+        # You need to have user type on the password as the parameter in CFN and then pass the password to Lambda using ResourceProperties
+        # More help @ https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html
+        # You can pass value as event data from cfn and then get it from event object e.g. event['ResourceProperties']['Password']
+        mongo_password = event['ResourceProperties']['MongoDBPassword']        
+        print("Creating ssm parameter Mongo Password")
+        ssm_client = boto3.client('ssm')
+        # create a ssm parameter
+        response = ssm_client.put_parameter(
+            Name='MongoDBAdminPassword',
+            Description='Password of the Mongo DB Server Admin',
+            Value=mongo_password,
+            KeyId=kmskeyid,
+            Type='SecureString',
+            Overwrite=True,
+            Tier='Standard'
+        )
+        
+        print("Finished creating ssm parameters for Mongo and RDS Password")
+
         
         security_group_id = ''
         for security_group in describe_security_groups_response['SecurityGroups']:
